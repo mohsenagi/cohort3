fdd48960e3abaf55378f92a5c965a38b2bc05b63
Mohsen Aghajani <mohsen.agi@gmail.com>
2019-10-16 09:14:56 -0600

    daily3 done

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -1,34 +1,18 @@
-/*	
-	Write the function that will create this output:
-
-*** the two values are not the same:
-    p1--> a
-    p2--> b
-*** the two values are not the same:
-    p1--> 1
-    p2--> 2
-*** the two values are not the same:
-    p1--> 2
-    p2--> 2
-*/
-
-// Write the function after this comment ---
-function compare(a, b) {
-    if (a === b) {
-        return true
-    }
-    console.log("the two values are not the same")
-    console.log(`p1 --> ${a}`)
-    console.log(`p2 --> ${b}`)
-    return false
+const functions = {
+    
+    makeEmailObj: (object1) => {
+        return `${object1.fname.toLowerCase()}.${object1.lname.toLowerCase()}@evolveu.ca`;
+    },
+    compare: (a, b) => {
+        if (a === b) return true;
+        console.log("the two values are not the same");
+        console.log(`p1 --> ${a}`);
+        console.log(`p2 --> ${b}`);
+        return false;
+    },
+    makeEmailArr: (array1) => {
+        return `${array1[0].toLowerCase()}.${array1[1].toLowerCase()}@evolveu.ca`;
+    },
 };
-export default compare;
 
-// and before this comment ---
-
-// assertEquals("a","b");
-// assertEquals("a","a");
-// assertEquals(1,2);
-// assertEquals(2,2);
-// assertEquals("2",2);
-// assertEquals("This value","This value");
+export default functions;
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -1,10 +1,28 @@
-import compare from './daily.js'
+import functions from './daily.js'
 
 test ('does compare work?', () => {
-    expect(compare(1, 2)).toBe(false);
-    expect(compare(10, 10)).toBe(true);
-    expect(compare(-1, -1)).toBe(true);
+    expect(functions.compare(1, 2)).toBe(false);
+    expect(functions.compare(10, 10)).toBe(true);
+    expect(functions.compare(-1, -1)).toBe(true);
 });
-test ("test the test", () => {
-    console.log("Hello World");
+
+test('email builder from an array', () => {
+    const name = ["first", "last"];
+    expect(functions.makeEmailArr(name))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["First", "Last"]))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailArr(["Bill", "Smith"]))
+        .toEqual("bill.smith@evolveu.ca");
+});
+
+
+test('email builder from an object / map', () => {
+    const name = { fname: 'first', lname: 'last' };
+    expect(functions.makeEmailObj(name))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailObj({ fname: 'First', lname: 'Last' }))
+        .toEqual("first.last@evolveu.ca");
+    expect(functions.makeEmailObj({ fname: "Bill", lname: "Smith" }))
+        .toEqual("bill.smith@evolveu.ca");
 });
