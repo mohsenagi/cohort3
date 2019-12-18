cf492cc5a969dde8bcc80659a66ddec82f18c73a
Mohsen Aghajani <mohsen.agi@gmail.com>
2019-12-17 22:58:58 -0700

    python TDD set up

---
+++ src/python/syntax.py
@@ -0,0 +1,2 @@
+def email(first, last):
+    return(f"{first.lower()}.{last.lower()}@evolveu.ca")
---
+++ src/python/test_syntax.py
@@ -0,0 +1,6 @@
+from syntax import *
+
+
+def test_email():
+    assert email('Larry', 'Shumlich') == 'larry.shumlich@evolveu.ca'
+    assert email('Mohsen', 'Aghajani') == 'mohsen.aghajani@evolveu.ca'
